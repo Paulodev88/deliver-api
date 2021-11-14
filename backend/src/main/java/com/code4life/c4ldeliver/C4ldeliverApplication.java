14cdec1f3b164424564ddac6c85565f5477d7efe
Paulo Santos <paulo.ricardodev88@gmail.com>
2021-11-14 16:43:34 -0300

    Project created

---
+++ backend/src/main/java/com/code4life/c4ldeliver/C4ldeliverApplication.java
@@ -0,0 +1,13 @@
+package com.code4life.c4ldeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C4ldeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C4ldeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/code4life/c4ldeliver/C4ldeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code4life.c4ldeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C4ldeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
