2259fb3adb22254a24c6df1c77fe9a6cd6586c0e
Paulodev88 <paulo.ricardodev88@gmail.com>
2021-11-14 18:01:08 -0300

    create: Class - Order / OrderStatus

---
+++ backend/src/main/java/com/code4life/c4ldeliver/entities/Order.java
@@ -0,0 +1,106 @@
+package com.code4life.c4ldeliver.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Order implements Serializable {
+		
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String address;
+	private double latitude;
+	private double longitude;
+	private Instant moment;
+	private OrderStatus status;
+	
+	private Set<Product> products = new HashSet<>();
+	
+	public Order() {}
+
+	public Order(Long id, String address, double latitude, double longitude, Instant moment, OrderStatus status) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+	
+
+}
---
+++ backend/src/main/java/com/code4life/c4ldeliver/entities/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.code4life.c4ldeliver.entities;
+
+public enum OrderStatus {
+		
+	PENDING,
+	DELIVERED;
+	
+}
