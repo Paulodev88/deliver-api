6ae1ca328dd148f5b5a97560f30da956905f0a4c
Paulodev88 <paulo.ricardodev88@gmail.com>
2021-11-14 18:38:16 -0300

    add: Object-relational mapping

--- backend/src/main/java/com/code4life/c4ldeliver/entities/Order.java
+++ backend/src/main/java/com/code4life/c4ldeliver/entities/Order.java
@@ -6,20 +6,39 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
 public class Order implements Serializable {
-		
+
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String address;
 	private double latitude;
 	private double longitude;
 	private Instant moment;
 	private OrderStatus status;
-	
+
+	@ManyToMany
+	@JoinTable(
+			name = "tb_order_product", 
+			joinColumns = @JoinColumn(name = "order_id"), 
+			inverseJoinColumns = @JoinColumn(name = "product_id"))
 	private Set<Product> products = new HashSet<>();
-	
-	public Order() {}
+
+	public Order() {
+	}
 
 	public Order(Long id, String address, double latitude, double longitude, Instant moment, OrderStatus status) {
 		super();
@@ -99,8 +118,5 @@ public class Order implements Serializable {
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	
-	
 
 }
